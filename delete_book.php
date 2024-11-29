fe0510e9bef1d5a884f612ad27f6890dd81cf1f0
Nikhil Gautam <monu31598@gmail.com>
2024-11-30 01:26:42 +0530

    Add files via upload

---
+++ delete_book.php
@@ -0,0 +1,14 @@
+<?php
+include 'db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM books WHERE id=$id";
+    if ($conn->query($sql) === TRUE) {
+        echo "Book deleted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+header("Location: index.php");
+?>
